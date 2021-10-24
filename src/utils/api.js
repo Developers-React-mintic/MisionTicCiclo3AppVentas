34e252200ba89c4559eca0adc37bb3ddf2a0a2b8
Jeferson <guevarajeferson@gmail.com>
2021-10-24 13:47:11 -0500

    Se corrige url del backend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const baseURL = "http://localhost:5000";
- const baseURL = "https://https://salty-lowlands-63689.herokuapp.com";
+ const baseURL = "https://salty-lowlands-63689.herokuapp.com";
 
 const getToken = () => {
   return `Bearer ${localStorage.getItem("token")}`;
