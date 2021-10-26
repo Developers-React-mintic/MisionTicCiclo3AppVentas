ed7d66ee2ff688ab964c259d28e10ab0ddad0444
Jeferson <guevarajeferson@gmail.com>
2021-10-26 00:45:41 -0500

    Se agrega tarjeta de perfil de usuario

--- src/pages/Admin/Perfil.jsx
+++ src/pages/Admin/Perfil.jsx
@@ -1,9 +1,48 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import { obtenerDatosUsuario, editarUsuario } from "utils/api";
-const Perfil = () => {
-  const { user } = useAuth0();
+//import { useAuth0 } from "@auth0/auth0-react";
+import { obtenerDatosUsuario } from "utils/api";
+import React, { useState, useEffect } from "react";
+import { Card } from "react-bootstrap";
 
-  return <div></div>;
+const Perfil = () => {
+  //const { user } = useAuth0();
+  const [usuario, setUsuario] = useState([]);
+  
+  useEffect(() => {
+    const fetchUsuario = async () => {
+      await obtenerDatosUsuario(
+        (response) => {
+          setUsuario(response.data);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    };
+    fetchUsuario();
+  }, []);
+  return <div className="center">
+     
+    
+    <Card style={{ width: '18rem'   }}>
+  <Card.Img variant="top" src={usuario.picture} />
+  <Card.Header as="h2">Perfil</Card.Header>
+
+  <Card.Body>
+  <Card.Title className="mb-2 text-muted">Nombre</Card.Title>
+    <Card.Text>{usuario.name}</Card.Text>
+  <Card.Subtitle className="mb-2 text-muted">Correo</Card.Subtitle>
+    <Card.Text>{usuario.email}</Card.Text>
+    
+    
+    <Card.Subtitle className="mb-2 text-muted">Creación</Card.Subtitle>
+    <Card.Text>{usuario.created_at}</Card.Text>
+    <Card.Subtitle className="mb-2 text-muted">Rol</Card.Subtitle>
+    <Card.Text>{usuario.rol}</Card.Text>
+    
+    
+  </Card.Body>
+</Card>
+  </div>;
 };
 
 export default Perfil;
